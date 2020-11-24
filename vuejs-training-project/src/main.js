5a7de02a3aa3637aa1cded9dc47efcfc923b2000
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-11-25 00:01:17 +0900

    単一ファイルコンポーネントを作成して、グローバル登録し描画する

--- vuejs-training-project/src/main.js
+++ vuejs-training-project/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+// 'LikeNumber.vue'をimportすることでオブジェクトとして取り扱う。
+import LikeNumber from './LikeNumber';
 
 Vue.config.productionTip = false;
+// 'LikeNumber'をグローバルコンポーネントとして登録する。
+Vue.component('LikeNumber', LikeNumber);
 
 new Vue({
   render: h => h(App),
