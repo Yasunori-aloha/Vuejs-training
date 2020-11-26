c920045a2b1a7abcf2c5504da732a1ed3511e9ce
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-11-26 14:42:57 +0900

    親コンポーネントから子コンポーネントへのデータの受け渡し方法

--- vuejs-training-project/src/App.vue
+++ vuejs-training-project/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <h2>{{ number }}</h2>
+    <LikeNumber :number='number'></LikeNumber>
+    <LikeNumber :number='number'></LikeNumber>
   </div>
 </template>
 
 <script>
 import LikeHeader from './components/LikeHeader.vue'
 export default {
+  data() {
+    return {
+      number: 26
+    }
+  },
   components:{
     LikeHeader
   }
--- vuejs-training-project/src/components/LikeHeader.vue
+++ vuejs-training-project/src/components/LikeHeader.vue
@@ -1,3 +1,4 @@
 <template>
-  <h1>いいね</h1>
+  <h1>トータルいいね数
+  </h1>
 </template>
