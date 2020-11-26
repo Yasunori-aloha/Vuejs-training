50ba877a71734126bcf915cea30fd34efcfcd159
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-11-26 20:03:47 +0900

    'componentタグ'を使用して動的に表示しているコンポーネントを切り替える方法

--- vuejs-training-project/src/App.vue
+++ vuejs-training-project/src/App.vue
@@ -2,45 +2,59 @@
   <div>
     <!-- 'templateタグ'の外に記述しているHTMLタグは<slot v-slot:default></slot>というタグに全てがまとめられる。 -->
     <LikeHeader header-text='Hello'>
-      <h3>{{ user }}</h3>
-      <h2>みなさん</h2>
+      <!-- <h3>{{ user }}</h3>
+      <h2>みなさん</h2> -->
 
       <!-- =''とすることによって子コンポーネントからデータを受け取ることができる。 -->
-      <template #[title]='slotProps' >
+      <!-- <template #[title]='slotProps' >
         <h1>トータルいいね数</h1>
         <h2>{{ slotProps.user.firstName }}</h2>
-      </template>
+      </template> -->
 
       <h3>こんにちは</h3>
 
-      <template #number>
+      <!-- <template #number>
         <h2>{{ number }}</h2>
       </template>
 
-      <p>よろしくお願いします</p>
+      <p>よろしくお願いします</p> -->
     </LikeHeader>
 
-    <LikeHeader #default="user">
+    <!-- <LikeHeader #default="user">
       <h3>{{ user }}</h3>
-    </LikeHeader>
+    </LikeHeader> -->
 
     <LikeNumber :total-number='number' test-props='test' @my-click='incrementNumber'></LikeNumber>
 
-    <LikeNumber :total-number='number' test-props='test' @my-click='number=$event'></LikeNumber>
+    <!-- <LikeNumber :total-number='number' test-props='test' @my-click='number=$event'></LikeNumber> -->
+
+    <!-- コンポーネントを切り替える為のボタン -->
+    <button @click='currentComponent = "Home"'>Home</button>
+    <button @click='currentComponent = "About"'>About</button>
+    <!-- 'is属性'を使用することでコンポーネントの切り替えを1行で済ませることができる。 -->
+    <component :is="currentComponent"></component>
+    <!-- <About v-if="currentComponent === 'About'"></About>
+    <Home v-if="currentComponent === 'Home'"></Home> -->
   </div>
 </template>
 
 <script>
 import LikeHeader from './components/LikeHeader.vue'
+import About from './components/About.vue'
+import Home from './components/Home.vue'
+
 export default {
   data() {
     return {
       number: 26,
-      title: 'title'
+      title: 'title',
+      currentComponent: 'Home'
     }
   },
   components:{
-    LikeHeader
+    LikeHeader,
+    About,
+    Home
   },
   methods: {
     incrementNumber(value){
--- vuejs-training-project/src/components/LikeHeader.vue
+++ vuejs-training-project/src/components/LikeHeader.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <!-- 'slotタグ'はpropsとは違い、HTMLタグ自体を子コンポーネントに受け渡すことができる。 -->
-    <slot name='title' :user='user' text='text'>
+    <!-- <slot name='title' :user='user' text='text'> -->
       <!-- 'slotタグ'の中身が無かった場合に表示する、デフォルト画面の内容を指定することができる。 -->
-      <h2>デフォルトタイトル</h2>
-    </slot>
-    <slot :user='user' text='text'></slot>
-    <hr>
+      <!-- <h2>デフォルトタイトル</h2> -->
+    <!-- </slot> -->
+    <!-- <slot :user='user' text='text'></slot> -->
+    <!-- <hr>
     <p>いいねの数</p>
     <slot name='number'></slot>
-    <p>{{ headerText }}</p>
+    <p>{{ headerText }}</p> -->
+    <slot></slot>
   </div>
 </template>
 
