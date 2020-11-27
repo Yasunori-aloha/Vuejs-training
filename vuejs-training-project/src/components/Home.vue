dd6e736bc8b94af0a19491a3f39acc3bf9c1f15b
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-11-27 16:57:35 +0900

    カスタムディレクティブをローカル登録する方法

--- vuejs-training-project/src/components/Home.vue
+++ vuejs-training-project/src/components/Home.vue
@@ -2,3 +2,14 @@
   <!-- データをオブジェクトの中に格納することで、複数データを受け渡すことができる。 -->
   <p v-border:solid.round.shadow="{width: '5px', color: 'red'}">Home</p>
 </template>
+
+<script>
+export default {
+  // 'Homeコンポーネント'のみで使用できるカスタムディレクティブを設定できる。
+  directives: {
+    border() {
+      // 'v-border'の中身を記述
+    }
+  }
+}
+</script>
