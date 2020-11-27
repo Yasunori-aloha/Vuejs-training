99e257304df7e253d94e0f3b75454ffa2687f1c7
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-11-27 16:19:04 +0900

    カスタムディレクティブの'bind'と'update'を省略記法を使って記述する方法

--- vuejs-training-project/src/main.js
+++ vuejs-training-project/src/main.js
@@ -6,25 +6,29 @@ import LikeNumber from './components/LikeNumber';
 Vue.config.productionTip = false;
 // 'LikeNumber'をグローバルコンポーネントとして登録する。
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive('border', {
-  bind(el, binding, vnode) {
-    console.log(el);// ディレクティブが初めて対象の要素に紐づいた時
-    console.log(binding);// ディレクティブが初めて対象の要素に紐づいた時
-    console.log(vnode);// ディレクティブが初めて対象の要素に紐づいた時
-  },
-  inserted() {
-    console.log('inserted');// 親Nodeに挿入された時
-  },
-  update() {
-    console.log('update');// コンポーネントが更新される度、子コンポーネントが更新される前
-  },
-  componentUpdated() {
-    console.log('componentUpdated');// コンポーネントが更新される度、子コンポーネントが更新された後
-  },
-  unbind() {
-    console.log('unbind');// ディレクティブが紐づいている要素から取り除かれた時
-  }
+// 'bind'と'update'をよく使用する為、関数を記述することでその2つのフック関数を1つのコードにまとめることができる。
+Vue.directive('border', function() {
+  console.log('test');
 })
+// Vue.directive('border', {
+//   bind(el, binding, vnode) {
+//     console.log(el);// ディレクティブが初めて対象の要素に紐づいた時
+//     console.log(binding);// ディレクティブが初めて対象の要素に紐づいた時
+//     console.log(vnode);// ディレクティブが初めて対象の要素に紐づいた時
+//   },
+  // inserted() {
+  //   // 親Nodeに挿入された時
+  // },
+  // update() {
+  //   // コンポーネントが更新される度、子コンポーネントが更新される前
+  // },
+  // componentUpdated() {
+  //   // コンポーネントが更新される度、子コンポーネントが更新された後
+  // },
+  // unbind() {
+  //   // ディレクティブが紐づいている要素から取り除かれた時
+  // }
+// })
 
 new Vue({
   render: h => h(App),
