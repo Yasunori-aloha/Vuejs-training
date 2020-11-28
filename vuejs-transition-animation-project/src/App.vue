2d86fc197930b871939d1c42e66cab5b179c7566
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-11-28 23:38:57 +0900

    'transitionコンポーネント'とCSSトランジションを使用したフェードイン・アウトの設定方法

--- vuejs-transition-animation-project/src/App.vue
+++ vuejs-transition-animation-project/src/App.vue
@@ -20,23 +20,29 @@ export default {
 
 <style scoped>
 /* 'transitionコンポーネント'を使用する場合は'name属性値'以下の'enter-active'等のクラスを計6個作成する必要がある。 */
-.fade-enter-active {
-/* 現れる時の最初の状態 */
-}
 .fade-enter {
-/* 現れる時のトランジションの状態 */
+  /* 現れる時の最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れる時のトランジションの状態 */
+  transition: opacity 5s;
 }
 .fade-enter-to {
-/* 現れる時の最後の状態 */
+  /* 現れる時の最後の状態 */
+  opacity: 1;
 }
 .fade-leave {
-/* 消える時の最初の状態 */
+  /* 消える時の最初の状態 */
+  opacity: 1;
 }
 .fade-leave-active {
-/* 消える時のトランジションの状態 */
+  /* 消える時のトランジションの状態 */
+  transition: opacity 5s;
 }
 .fade-leave-to {
-/* 消える時の最後の状態 */
+  /* 消える時の最後の状態 */
+  opacity: 0;
 }
 .main{
   width: 70%;
