24bef7642ad8de9fd7a1153ce075e3cdea177f44
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-11-28 23:52:20 +0900

    'トランジション用クラス'がどのタイミングでクラスを追加・削除しているのかについて

--- vuejs-transition-animation-project/src/App.vue
+++ vuejs-transition-animation-project/src/App.vue
@@ -20,14 +20,17 @@ export default {
 
 <style scoped>
 /* 'transitionコンポーネント'を使用する場合は'name属性値'以下の'enter-active'等のクラスを計6個作成する必要がある。 */
+/* 最初にクラスが追加され初期値を設定、その後1フレーム後にはクラスを削除する。 */
 .fade-enter {
   /* 現れる時の最初の状態 */
   opacity: 0;
 }
+/* 最初にクラスが追加され、'transitionendイベント'で変化が終わったことを受け取ったらクラスを削除する。 */
 .fade-enter-active {
   /* 現れる時のトランジションの状態 */
-  transition: opacity 5s;
+  transition: opacity 0.5s;
 }
+/* 'enter'で初期値が設定された後にクラスが追加されて、'transitionendイベント'で変化が終わったことを受け取ったらクラスを削除する。 */
 .fade-enter-to {
   /* 現れる時の最後の状態 */
   opacity: 1;
@@ -38,7 +41,7 @@ export default {
 }
 .fade-leave-active {
   /* 消える時のトランジションの状態 */
-  transition: opacity 5s;
+  transition: opacity 0.5s;
 }
 .fade-leave-to {
   /* 消える時の最後の状態 */
