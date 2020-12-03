37f3090bf7a8407497eab60978c62363a58951e1
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-03 17:09:25 +0900

    ''を使用して、アクセスするページを変更する方法

--- vuejs-vuerouter-training/src/views/Home.vue
+++ vuejs-vuerouter-training/src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h3>Home</h3>
+    <button @click='toUsers'>Usersのページに遷移する</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    toUsers() {
+      // '$router'で'view-router'にアクセスすることができる。
+      this.$router.push('users');
+    },
+  },
+}
+</script>
