5ac4cad4a4dd77b3cf54531900c4d40746587b8d
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-03 16:58:37 +0900

    'active-class属性'や'exact属性'を使用した、表示されている'active-link'にのみクラスを適用させる方法

--- vuejs-vuerouter-training/src/App.vue
+++ vuejs-vuerouter-training/src/App.vue
@@ -3,8 +3,10 @@
     <nav>
       <!-- 'router-linkタグ'を使用することで、'router-view'の中に入っているコンポーネントを動的に切り替えることができるため、ページ遷移が高速。 -->
       <!-- 初期は'aタグ'で表示する様になっているが、'tag属性'を付与することで任意のタグに変化させることができる。 -->
-      <router-link to="/" class='link'>Home</router-link>
-      <router-link to="/users" class='link'>Users</router-link>
+      <!-- 'active-class属性'を使用することで、付与した'router-link'が表示されている時に指定したクラスを適用させることができる。 -->
+      <!-- 'exact属性'を付与すると、URLが完全一致しない限りはアクティブにならない様に設定することができる。 -->
+      <router-link to="/" active-class='link__active' exact class='link'>Home</router-link>
+      <router-link to="/users" active-class='link__active' exact class='link'>Users</router-link>
     </nav>
     <!-- 最初は'App.vue'を表示させてしまうため、'router-viewタグ'を使用することで、'router.js'で定義したルーティングを適用させる。 -->
     <router-view></router-view>
@@ -12,7 +14,10 @@
 </template>
 
 <style scoped>
-  .link {
-    margin-right: 10px;
-  }
+.link {
+  margin-right: 10px;
+}
+.link__active {
+  font-size: 20px;
+}
 </style>
