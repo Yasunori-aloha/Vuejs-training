88c45a9c7b4571935f43a9590fb3a1cdacb6f7bd
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-03 16:50:09 +0900

    'router-link'を使用したページ読み込み無しのページ遷移方法

--- vuejs-vuerouter-training/src/App.vue
+++ vuejs-vuerouter-training/src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
+  <div style='width: 700px; margin: auto; padding-top: 50px;'>
+    <nav>
+      <!-- 'router-linkタグ'を使用することで、'router-view'の中に入っているコンポーネントを動的に切り替えることができるため、ページ遷移が高速。 -->
+      <!-- 初期は'aタグ'で表示する様になっているが、'tag属性'を付与することで任意のタグに変化させることができる。 -->
+      <router-link to="/" class='link'>Home</router-link>
+      <router-link to="/users" class='link'>Users</router-link>
+    </nav>
     <!-- 最初は'App.vue'を表示させてしまうため、'router-viewタグ'を使用することで、'router.js'で定義したルーティングを適用させる。 -->
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+</style>
