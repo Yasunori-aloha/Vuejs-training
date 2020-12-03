ab976be80cf83ff2de788ea73acdf5782671ff2e
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-03 16:16:05 +0900

    'mode:hisotry'を使用した、シングルページアプリケーションでのURL設定方法

--- vuejs-vuerouter-training/src/router.js
+++ vuejs-vuerouter-training/src/router.js
@@ -8,5 +8,7 @@ Vue.use(Router)
 
 // どのURLにアクセスした時に、どのコンポーネントを表示させるのかを決めるための記述。
 export default new Router({
+  // 'mode'を'history'にすることで、URLを'/#/'からでは無く'/'で始まる様に変更できる。ただし、ページ遷移するごとにページ読み込みが挟まる。
+  mode: 'history',
   routes: [{path: '/', component: Home}, {path: '/users', component: Users}]
 });
