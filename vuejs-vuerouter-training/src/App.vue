91e965d61409d1058a83f37ccacdc2cfc6d1229c
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-04 20:13:52 +0900

    'router-view'にトランジションを適用する方法

--- vuejs-vuerouter-training/src/App.vue
+++ vuejs-vuerouter-training/src/App.vue
@@ -2,6 +2,20 @@
   <div style="width: 700px; margin: auto; padding-top: 50px">
     <!-- 最初は'App.vue'を表示させてしまうため、'router-viewタグ'を使用することで、'router.js'で定義したルーティングを適用させる。 -->
     <router-view name="header"></router-view>
-    <router-view></router-view>
+    <transition name='fade' mode='out-in'>
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
+
+<style scoped>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+</style>
