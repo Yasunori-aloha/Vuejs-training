856f8eadbcb0e44ac43b6974b6693c0e43a6f02c
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-04 21:49:51 +0900

    全てのページの遷移前に処理を実行させる方法

--- vuejs-vuerouter-training/src/main.js
+++ vuejs-vuerouter-training/src/main.js
@@ -6,6 +6,13 @@ import router from './router';
 
 Vue.config.productionTip = false
 
+// 全てのページで、遷移前に実行したい処理をこの中に記述する。
+router.beforeEach((to, from, next) => {
+  // console.log('global-beforeEach');
+  // ()の中には'false'や'パス'を入力する。falseなら次のページに遷移しない処理ができる。
+  next();
+})
+
 new Vue({
   // 'Vue.use(Router)'を記述したことで'router'というキーが使用可能になっている。
   router,
