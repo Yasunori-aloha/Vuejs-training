393cec5da26efe36396884d5c7a210fdd99939ab
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-04 20:26:14 +0900

    URLに'#'を付与する方法

--- vuejs-vuerouter-training/src/views/Users.vue
+++ vuejs-vuerouter-training/src/views/Users.vue
@@ -6,8 +6,12 @@
     <hr />
     <!-- '$route'を使用することで、パラメーターを取得することができる。 -->
     <h1>User No. {{ id }}</h1>
-    <router-link :to="'/users/' + (Number(id) + 1) + '/profile?lang=ja'">次のユーザー</router-link>
-    <router-link :to="{ name: 'users-id-profile', params: { id: (Number(id) + 1)}, query: { lang: 'ja', page: 2 } }">次のユーザー</router-link>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile?lang=ja#next-user'">次のユーザー</router-link>
+
+    <div style='height: 700px;'></div>
+    <!-- 'id'を付与することで、'#'を使ったページスクロールができる。 -->
+    <!-- 'hash'を使用することで、クリックした際に指定した'id'のある要素に飛んでいく。 -->
+    <router-link id='next-user' :to="{ name: 'users-id-profile', params: { id: (Number(id) + 1)}, query: { lang: 'ja', page: 2 }, hash: '#next-user' }">次のユーザー</router-link>
   </div>
 </template>
 
