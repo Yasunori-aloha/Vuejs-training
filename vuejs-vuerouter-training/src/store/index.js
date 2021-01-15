b08559d49ed035f636ed29288c9f53a38d427357
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2021-01-15 23:44:08 +0900

    add: 'getters'や'mutations'の内容ごとにファイルを作成しコードを切り出す方法

--- vuejs-vuerouter-training/src/store/index.js
+++ vuejs-vuerouter-training/src/store/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+// 機能ごとにコードを切り出し
 import count from "./modules/count";
+// 'getters'や'mutations'それぞれを別ファイルに切り出し
+import getters from './getters';
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
@@ -9,19 +14,9 @@ export default new Vuex.Store({
     message: ""
   },
   // 同じ関数を複数のコンポーネントで使用する際に、まとめておいて使用できる。
-  getters: {
-    message: state => state.message
-  },
-  mutations: { // 'mutations'は'getters'と違い引数を複数取ったり、値を'return'で返す必要がないため、アロー関数にしなくてもいい。
-    updateMessage(state, newMessage) {
-      state.message = newMessage;
-    }
-  },
-  actions: {
-    updateMessage({ commit }, newMessage) {
-      commit("updateMessage", newMessage);
-    }
-  },
+  getters,
+  mutations,// 'mutations'は'getters'と違い引数を複数取ったり、値を'return'で返す必要がないため、アロー関数にしなくてもいい。
+  actions,
   modules: {
     count
   }
